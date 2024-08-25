5ca20ded521170493d918b12719d1b4b9b20b78e
sean <siryle1213@gmail.com>
2024-08-25 03:30:34 +0000

    modified:   Scripts/Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -51,6 +51,6 @@ if [[ $WRT_REPO == *"lede"* ]]; then
 else
 	echo "CONFIG_PACKAGE_luci=y" >> ./.config
 	echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
+##	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
  	echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config
 fi
