5f8178135bc87e2b4d4242a6c39e127b3c3e042b
sean <siryle1213@gmail.com>
2024-08-25 02:10:55 +0000

    modified:   Scripts/Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -37,7 +37,7 @@ UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"
 
 if [[ $WRT_REPO != *"lede"* ]]; then
 	UPDATE_PACKAGE "daed" "QiuSimons/luci-app-daed" "master"
-	UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
+	UPDATE_PACKAGE "homeproxy" "immortalwrt/homeproxy" "master"
 	UPDATE_PACKAGE "mihomo" "morytyann/OpenWrt-mihomo" "main" "pkg"
 
 	UPDATE_PACKAGE "alpha" "derisamedia/luci-theme-alpha" "master"
